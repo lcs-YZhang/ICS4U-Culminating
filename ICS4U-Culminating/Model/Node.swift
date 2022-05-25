c15d3af82b4f86dac9c41ba2b4b8a7d06eb2b305
Devon Hansen <dhansen@lcs.on.ca>
2022-05-25 10:11:48 -0400

    Added graph and model groups and started work on the side structure.

---
+++ ICS4U-Culminating/Model/Node.swift
@@ -0,0 +1,15 @@
+//
+//  Node.swift
+//  ICS4U-Culminating
+//
+//  Created by Devon Kenneth Hansen on 2022-05-25.
+//
+
+import Foundation
+
+struct Node: Identifiable {
+    let id: Int                  //The node id (e.g.: 1, 2, 3, etc)
+    let paragraphs: [String]     //Models Paragraphs on a page of the book
+    let image: String?           //name of image from assests oflder if one exists
+    let edges: [Int]             //a list of id's of the nodes this node is connected to
+}
