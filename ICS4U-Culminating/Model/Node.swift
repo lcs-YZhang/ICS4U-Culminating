5a9af8f1056b1b029ecdf2b0f9667f2a81ef8399
Devon Hansen <dhansen@lcs.on.ca>
2022-05-27 09:33:25 -0400

    did nil's for images

--- ICS4U-Culminating/Model/Node.swift
+++ ICS4U-Culminating/Model/Node.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct Node: Identifiable {
     let id: Int                  //The node id (e.g.: 1, 2, 3, etc)
-    let paragraphs: [String]     //Models Paragraphs on a page of the book
-    let image: String?           //name of image from assests oflder if one exists
+    let paragraphs: [nil]     //Models Paragraphs on a page of the book
+    let image: nil?           //name of image from assests oflder if one exists
     let edges: [Int]             //a list of id's of the nodes this node is connected to
 }
 
@@ -21,27 +21,227 @@ let nodes = [
                       "I need. I need... says a weak voice. You hear a loud click, and the phone goes dead.Drat! You weren't ready for that. You, theaspiring detective and psychic investigator, caughtoff guard. You slump down into your chair. Thatvoice really sounded desperate.",
                      "You boot up your computer and look around.The heat of the day has not penetrated yourcombination office and research laboratory, whereyou sit surrounded by the tools of your trade:infrared activated floodlights,high speedmoviecameras, and night scopes among them. Two largebookcases stretch from floor to ceiling, crammedwith titles that would drive the timid from the room:Murder in Fun, Ghosts and Ghouls, and CorpsesI Have Known, to name a few.",
                      ],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 2,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [6]
     ),
     Node(id: 6,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [7]
     ),
     Node(id: 7,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [4]
     ),
     Node(id: 1,
          paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 2,
+         paragraphs: [],
          image: <#T##String?#>,
+         edges: [6]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 2,
+         paragraphs: [],
+         image: nil,
+         edges: [6]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
+         edges: [2]
+    ),
+    Node(id: 1,
+         paragraphs: [],
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
@@ -181,292 +381,92 @@ let nodes = [
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 2,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [6]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 2,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [6]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     ),
     Node(id: 1,
          paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 2,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [6]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
-         edges: [2]
-    ),
-    Node(id: 1,
-         paragraphs: [],
-         image: <#T##String?#>,
+         image: nil,
          edges: [2]
     )
 ]
